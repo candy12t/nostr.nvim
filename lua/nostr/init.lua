84888c4e752a836214cfcb6cc282d2f24575e993
Takumi Kanada <candy12t@gmail.com>
2024-02-13 20:36:04 +0900

    Add: nostr post

--- lua/nostr/init.lua
+++ lua/nostr/init.lua
@@ -1,9 +1,22 @@
 local M = {}
 
+-- TODO: implement timeline
+local timeline = function()
+  print("Nostr timeline")
+end
+
+local post = function(text)
+  vim.fn.system({ "algia", "post", text })
+end
+
 function M.setup()
-	vim.api.nvim_create_user_command("Nostr", function()
-		print("Hello, Nostr!")
-	end, {})
+  vim.api.nvim_create_user_command("Nostr", function()
+    timeline()
+  end, {})
+
+  vim.api.nvim_create_user_command("NostrPost", function(opts)
+    post(opts.args)
+  end, { nargs = "+" })
 end
 
 return M
